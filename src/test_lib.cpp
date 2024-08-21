86bebe598cd5d1cf27bf16e72b0e0b95a08b91d4
SYaoJun <940334249@qq.com>
2024-08-21 17:12:16 +0800

    chore(tidy): remove redundant code

--- src/test_lib.cpp
+++ src/test_lib.cpp
@@ -1,22 +1,15 @@
 
-//
-// Created by William.Hua on 2021/7/31.
-//
-
-bool is_power_of_two(int n)
-{
+bool is_power_of_two(int n){
     if (n < 0)
         return false;
 
     return (n & (n - 1)) == 0;
 }
 
-int multiply_by_two(int n)
-{
+int multiply_by_two(int n){
     return n << 1;
 }
 
-int divide_by_two(int n)
-{
+int divide_by_two(int n){
     return n >> 1;
 }
--- test/test_hellocov.cpp
+++ test/test_hellocov.cpp
@@ -3,16 +3,10 @@
 
 #include "gtest/gtest.h"
 
-int add(int a, int b) {
-    return a + b;
-}
 
 TEST(AddFunctionTest, PositiveNumbers) {
     is_power_of_two(8);
     multiply_by_two(7);
     divide_by_two(-7);
     is_power_of_two(-8);
-    EXPECT_EQ(add(2, 3), 5);
-    EXPECT_EQ(add(0, 0), 0);
-    EXPECT_EQ(add(-1, 1), 0);
 }
